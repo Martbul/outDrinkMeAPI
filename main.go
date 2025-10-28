927db72d163fad746140fa9742eb043a7077838f
Martbul <martbul01@gmail.com>
2025-10-28 11:01:34 +0200

    fix delete account details route

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func main() {
 	api.HandleFunc("/privacy-policy", docHandler.ServePrivacyPolicy).Methods("GET")
 	api.HandleFunc("/terms-of-services", docHandler.ServeTermsOfServices).Methods("GET")
 api.HandleFunc("/delete-account-webpage", userHandler.DeleteAccountPage).Methods("GET")
-api.HandleFunc("/delete-account-webpage", userHandler.UpdateAccountPage).Methods("GET")
+api.HandleFunc("/delete-account-details-webpage", userHandler.UpdateAccountPage).Methods("GET")
 
 
 	// Protected API routes (auth required)
